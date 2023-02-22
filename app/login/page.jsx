8e9dda04a078f7dd01c939f9de65fd132b74c829
GiancarloCabrera <giancarlo.cabrera@unac.edu.co>
2023-02-21 20:20:39 -0500

    Integrating Material-UI

---
+++ app/login/page.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+// 'use client'
+
+export default function Home() {
+
+  return (
+    <>
+        <h1>LOGIN PAGE</h1>    
+    </>
+  )
+}
