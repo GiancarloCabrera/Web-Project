3b327f3957b2a2e021385c68ad1a9a4e39f4c7c9
GiancarloCabrera <giancarlo.cabrera@unac.edu.co>
2023-06-04 20:43:23 -0500

    formating project

--- app/(private-routes)/layout.jsx
+++ app/(private-routes)/layout.jsx
@@ -1,12 +1,7 @@
-import ResponsiveDrawer from "../../components/Drawer";
-
 export default async function PrivateRoutesLayout({children}) {    
     return (
      <div>
-          <ResponsiveDrawer>
-            {children}
-          </ResponsiveDrawer>
-          
+        {children}          
       </div>
     )
 }
